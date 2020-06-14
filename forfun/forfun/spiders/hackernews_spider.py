2b9f4a044057db1c06d2dbcaab8e8605767a4f75
Gordon Wall <gordonwall9@gmail.com>
2020-06-13 23:13:32 -0400

    adding Selector usage to spider

--- forfun/forfun/spiders/hackernews_spider.py
+++ forfun/forfun/spiders/hackernews_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.selector import Selector
 
 
 class HackernewsSpider(scrapy.Spider):
@@ -7,14 +8,20 @@ class HackernewsSpider(scrapy.Spider):
     start_urls = ['https://news.ycombinator.com/']
 
     def parse(self, response):
+        news = Selector(response)
+        
+
+
         for news in response.xpath('//table[@class="itemlist"]'):
             yield {
-                'title': news.xpath('.//a[@class="storylink"]/text()').getall(),
-                'source': news.xpath('.//a[@class="storylink"]/@href').getall(),
-                'points': news.xpath('.//span[@class="score"]/text()').getall()
+
+                'title': news.xpath('.//a[@class="storylink"]/text()').extract(),
+                'source': news.xpath('.//a[@class="storylink"]/@href').extract(),
+                'points': news.xpath('.//span[@class="score"]/text()').extract()
+
             }
 
-        next_page = response.xpath('.//a[@class="morelink"]/@href').get()
+        next_page = response.xpath('.//a[@class="morelink"]/@href').extract()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)

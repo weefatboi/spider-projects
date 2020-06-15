602c9cdcd5462359bffd65519af6519b4a8ee26f
Gordon Wall <gordonwall9@gmail.com>
2020-06-15 18:56:49 -0400

    updated twitter_spider
    working on correctly calling the start url without being forbidden by twitter robots.txt

--- forfun/forfun/spiders/twitter_spider.py
+++ forfun/forfun/spiders/twitter_spider.py
@@ -4,19 +4,26 @@ import scrapy
 class TwitterSpider(scrapy.Spider):
     name = "twitter"
 
-    allowed_domains = [
-
-        "https://twitter.com/",
-        "https://mobile.twitter.com/",
-
-    ]
-
     start_urls = [
 
-        "https://twitter.com/explore"
+        "https://twitter.com/search?q=%23finance&src=typed_query"
 
     ]
 
+    def parse(self, response):
+        text_body = response.xpath('.//div[@data-testid="tweet"]//span[@class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0"]/text()').extract()
+        username = response.xpath('.//div[@class="css-1dbjc4n r-zl2h9q"]//span[@class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0"]/text()').extract()
+        
+        import pdb; pdb.set_trace()
+        for item in zip(text_body, username):
+            finance_data = {
+
+                'text':item[0],
+                'username':item[1],
+
+            }
+
+            yield finance_data
     
 
 

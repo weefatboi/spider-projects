6e85769ea04d46125c62ba9572d6d84b0990e379
Gordon Wall <gordonwall9@gmail.com>
2020-06-24 18:01:54 -0400

    base twitter spider written

--- forfun/forfun/spiders/twitter_spider.py
+++ forfun/forfun/spiders/twitter_spider.py
@@ -6,24 +6,14 @@ class TwitterSpider(scrapy.Spider):
 
     allowed_domains = ['twitter.com']
 
+    # url encoding changes hashtag to %23 in twitter's base search url
+    # double percent signs used to escape
     def __init__(self, category=None, *args, **kwargs):
         super(TwitterSpider, self).__init__(*args, **kwargs)
-        self.start_urls = ['https://www.amazon.com/s?k=%s' % category]
+        self.start_urls = ['https://twitter.com/search?q=%%23%s' % category]
 
     def parse(self, response):
-        text_body = response.xpath('.//div[@data-testid="tweet"]//span[@class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0"]/text()').extract()
-        username = response.xpath('.//div[@class="css-1dbjc4n r-zl2h9q"]//span[@class="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0"]/text()').extract()
-        
-        import pdb; pdb.set_trace()
-        for item in zip(text_body, username):
-            finance_data = {
-
-                'text':item[0],
-                'username':item[1],
-
-            }
-
-            yield finance_data
+        pass
     
 
 

b421591f8b2257f9c2d4ebc5753469372fcf5e47
Gordon Wall <gordonwall9@gmail.com>
2020-06-25 14:27:42 -0400

    added format string to url
    customizable searches, by city and state

--- forfun/forfun/spiders/homefinder_spider.py
+++ forfun/forfun/spiders/homefinder_spider.py
@@ -7,6 +7,15 @@ from pprint import pprint
 class HomeFinderSpider(scrapy.Spider):
     name = 'homefinder'
 
-    
+    allowed_domains = ['homefinder.com']
+
+    start_urls = ['homefinder.com/for-sale/']
+
+    def parse(self, response):
+        city = 'Kent'
+        state = 'OH'
+
+        url = ('https://api.homefinder.com/v1/listing?scope=index&term={},+{}&'
+                'area=%%7B%%22city%%22:%%22{}%%22,%%22state%%22:%%22{}%%22%%7D').format(city, state, state, city)
 
 

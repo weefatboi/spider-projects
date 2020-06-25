009336aa2f977f1992baffb18cabfdab6d536266
Gordon Wall <gordonwall9@gmail.com>
2020-06-25 14:51:51 -0400

    defined parse details

--- forfun/forfun/spiders/homefinder_spider.py
+++ forfun/forfun/spiders/homefinder_spider.py
@@ -9,7 +9,18 @@ class HomeFinderSpider(scrapy.Spider):
 
     allowed_domains = ['homefinder.com']
 
-    start_urls = ['homefinder.com/for-sale/']
+    start_urls = ['https://homefinder.com/for-sale/']
+
+    headers = {
+
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://homefinder.com/",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        
+    }
 
     def parse(self, response):
         city = 'Kent'
@@ -17,5 +28,32 @@ class HomeFinderSpider(scrapy.Spider):
 
         url = ('https://api.homefinder.com/v1/listing?scope=index&term={},+{}&'
                 'area=%%7B%%22city%%22:%%22{}%%22,%%22state%%22:%%22{}%%22%%7D').format(city, state, state, city)
+
+        yield scrapy.Request(url, callback=self.parse_detail, headers=self.headers)
+
+    def parse_detail(self, response):
+        result = json.loads(response.body)
+
+        detail = {}
+
+        detail['city'] = result['listings']['city']
+        detail['state'] = result['listings']['state']
+        detail['zip'] = result['listings']['zip']
+        detail['address'] = result['listings']['addressLine1']
+        detail['year_built'] = result['listings']['yearBuilt']
+        detail['bed'] = result['listings']['bed']
+        detail['bath'] = result['listings']['bath']
+        detail['type'] = result['listings']['propertyType']
+        detail['price'] = result['listings']['price']
+        detail['sqft'] = result['listings']['squareFootage']
+        detail['desc'] = result['listings']['description']
+        detail['nbhd'] = result['listings']['neighborhood']
+
+        return detail
+        
+        
+        
+        
+
 
 

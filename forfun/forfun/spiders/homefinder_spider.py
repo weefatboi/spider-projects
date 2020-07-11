327bae363568dd37495293103ef5d239dae7f3d1
Gordon Wall <gordonwall9@gmail.com>
2020-07-10 20:03:26 -0400

    FIXED json output for homefinder

--- forfun/forfun/spiders/homefinder_spider.py
+++ forfun/forfun/spiders/homefinder_spider.py
@@ -7,6 +7,7 @@ class HomeFinderSpider(scrapy.Spider):
     name = 'homefinder'
 
     headers = {
+        
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
         }
@@ -23,59 +24,29 @@ class HomeFinderSpider(scrapy.Spider):
 
         yield scrapy.Request(start_urls, headers=self.headers, callback=self.parse)
 
-    #     headers = {
-    #         'Accept': 'application/json, text/plain, */*',
-    #         'Accept-Encoding': 'gzip, deflate, br',
-    #         'Accept-Language': 'en-US,en;q=0.9',
-    #         'Cache-Control': 'no-cache',
-    #         'Connection': 'keep-alive',
-    #         'Host': 'api.homefinder.com',
-    #         'Origin': 'https://homefinder.com',
-    #         'Pragma': 'no-cache',
-    #         'Referer': 'https://homefinder.com/',
-    #         'Sec-Fetch-Dest': 'empty',
-    #         'Sec-Fetch-Mode': 'cors',
-    #         'Sec-Fetch-Site': 'same-site',
-    #     }
-
-    #     yield scrapy.Request(url, callback=self.parse, headers=headers)
-
 
     def parse(self, response):
         result = json.loads(response.body)
-        print(result)
 
-        local = result['listings']['city']
-        state_abrv = result['listings']['state']
-        zipcode = result['listings']['zip']
-        address = result['listings']['addressLine1']
-        year_built = result['listings']['yearBuilt']
-        bed = result['listings']['bed']
-        bath = result['listings']['bath']
-        ptype = result['listings']['propertyType']
-        price = result['listings']['price']
-        sqft = result['listings']['squareFootage']
-        desc = result['listings']['description']
-        nbhd = result['listings']['neighborhood']
+        for listing in result['listings']:
 
-        for item in zip(local, state_abrv, zipcode, address, year_built, bed, bath, ptype, price, sqft, desc, nbhd):
-            listing_data = {
-                'city':item[0],
-                'state':item[1],
-                'zipcode':item[2],
-                'address':item[3],
-                'year_built':item[4],
-                'bed':item[5],
-                'bath':item[6],
-                'ptype':item[7],
-                'price':item[8],
-                'sqft':item[9],
-                'desc':item[10],
-                'nbhd':item[11],
+            yield {
+                'city':listing['city'],
+                'state':listing['state'],
+                'zipcode':listing['zip'],
+                'address':listing['addressLine1'],
+                'year_built':listing['yearBuilt'],
+                'bed':listing['bed'],
+                'bath':listing['bath'],
+                'ptype':listing['propertyType'],
+                'price':listing['price'],
+                'sqft':listing['squareFootage'],
+                'desc':listing['description'],
+                'nbhd':listing.get('neighborhood'),
             } 
 
-            yield listing_data
-            
+
+                
         
         
         
